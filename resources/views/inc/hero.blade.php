cea6d006dbd1b2f31d8abe6f855aaff294a37590
adiletyerkin <60466005+adiletyerkin@users.noreply.github.com>
2020-04-16 23:59:50 +0600

    Update hero.blade.php

--- resources/views/inc/hero.blade.php
+++ resources/views/inc/hero.blade.php
@@ -1,6 +1,6 @@
 <div class="jumbotron">
 	<div class="container">
 		<h1>МЫ ОФИЦИАЛЬНЫЕ ПРЕДСТАВИТЕЛИ БРЕНДОВ НА ТЕРРИТОРИИ КАЗАХСТАНА</h1>
-		<p>Something</p>
+		<p>Something else</p>
 	</div>
 </div>
