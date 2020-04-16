ed7055dc8134ce3e66a64b16088fcea402b874e1
adiletyerkin <60466005+adiletyerkin@users.noreply.github.com>
2020-04-17 03:17:57 +0600

    for login

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::get('/registration', function () {
 });
 
 Route::post('/registration/submit', 'RegistrationController@submit' )->name('contaact-form');
+
+Route::get('/login', function () {
+    return view('login');}->name('login');
+
