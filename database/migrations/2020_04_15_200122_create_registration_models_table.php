2a4715b92bed2ecb1504e4b3f35fb232009d45b4
YerUlan <51732861+turgan6ek@users.noreply.github.com>
2020-04-17 02:47:21 +0600

    Update 2020_04_15_200122_create_registration_models_table.php

--- database/migrations/2020_04_15_200122_create_registration_models_table.php
+++ database/migrations/2020_04_15_200122_create_registration_models_table.php
@@ -18,6 +18,7 @@ class CreateRegistrationModelsTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone');
+            $table->string('password');
             $table->timestamps();
 
         });
